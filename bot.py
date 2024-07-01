6649994aedf80d938cd73c647370faf02600162e
J3lackk <145368957+J3lackk@users.noreply.github.com>
2024-07-01 20:47:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,34 @@ app = Client(
 async def start(client, message):
     await message.reply("Ben test deneme botuyum")
 
-    
-
+    @app.on_message(filters.command("botcum") &filters .private)
+    async def botcum (client, mesaage):
+        if message .form_user.id ==
+    6905940236:
+             await message.reply_text("**
+    Sayın böcük hanım şuan çalışmaltayım 🇹🇷🤍**")
+         elif message .form_user.id ==
+    7142242630:
+            await
+    message.reply("sassy abla şuan çalışıyorum Merak etme 🥺👉👈**")
+          if message .form_user.id ==
+    7131686379:
+            await message.reply_text("** 
+     tırrek lili seviliyorsun bilesinnn🌸💕**")
+           if message .form_user.id ==
+    7182074621:
+            await message.reply_text("**
+            sahibimin küçük adamı🩵**")
+            if message .form_user.id ==
+    2040437974:
+            await message.reply_text("**
+            sayın hocamız değerli hocamız✨🤍**")
+            if message .form_user.id ==
+    6604549799:
+            await message.reply_text("** dayımda dayım baş 
+            tacımız 👑❤️**")
+            if mesaage .fırm_user.id ==
+    6716279900:
+            await message.reply_next("** sek sek seqooo 🌸❤️‍🔥**")
 
 app.run()
