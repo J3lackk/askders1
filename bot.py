9233dc771c32d9b03e35713959fa08cf58e19903
J3lackk <145368957+J3lackk@users.noreply.github.com>
2024-07-11 14:03:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ def welcome(client, message): # hoş geldin mesajı fonksiyonu tanımlıyoruz
 
 # bir kullanıcı gruptan ayrıldığında çalışacak fonksiyon 
 @app.on_message(filters.left_chat_member)
-def goodbye(client, mesaage):
+def goodbye(client, message):
     member = message.left_chat_member
     if member.id == OWNER_ID:
         message.reply(f"maalesef, {member.mention} gruptan ayrıldı. umarım tekrar gelirsin.! ")
